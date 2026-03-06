9ad965a0016e589719d41af0e1596f641d7e82e3
CarlosVillarinho <worksvillarinho@gmail.com>
2026-03-06 11:42:06 -0300

    Atualizando informacoes

---
+++ src/main/java/com/github/carlosvillarinho/ms/pedidos/entities/ItemDoPedido.java
@@ -0,0 +1,15 @@
+package com.github.carlosvillarinho.ms.pedidos.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@EqualsAndHashCode(of = "id")
+//@Entity
+//@Table(name = "tb_item_do_pedido")
+public class ItemDoPedido {
+}
